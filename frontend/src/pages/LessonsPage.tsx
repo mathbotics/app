1920afa331acd2ad3674332653822f9f59286dc6
David Castaneda <davidcastaneda@Davids-MBP.hsd1.fl.comcast.net>
2020-03-22 21:01:13 -0400

    add lesson preview

--- frontend/src/pages/LessonsPage.tsx
+++ frontend/src/pages/LessonsPage.tsx
@@ -1,10 +1,87 @@
 import React, { useState } from "react";
-import { Typography, Button, Modal, Alert } from "antd";
+import { Typography, Button, Modal, Alert, Layout, Tooltip } from "antd";
+import { ClockCircleOutlined, EditOutlined } from "@ant-design/icons";
 import { CreateLessonForm } from "../components/form";
 import { Store, ValidateErrorEntity } from "rc-field-form/lib/interface";
-import { PropertySafetyFilled } from "@ant-design/icons";
+import styled from "styled-components";
 
 const { Title } = Typography;
+
+const LessonPreviewWrapper = styled(Layout.Content)`
+  height: auto;
+  width: 400px;
+  border: 1px solid #ccc;
+  margin: 20px;
+  :hover {
+    color: inherit;
+  }
+`;
+
+const LessonsWrapper = styled(Layout.Content)`
+  display: flex;
+  justify-content: end;
+  flex-wrap: wrap;
+`;
+
+type LessonPreviewProps = {
+  id: string;
+  title: string;
+  slideCount: number;
+  onClick: any;
+};
+const LessonPreview = ({
+  id,
+  title,
+  slideCount,
+  onClick
+}: LessonPreviewProps) => {
+  return (
+    <LessonPreviewWrapper>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "end",
+          flexDirection: "column",
+          padding: "15px 25px"
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            width: "100%"
+          }}
+        >
+          <Tooltip title={title}>
+            <h1
+              style={{
+                fontSize: "30px",
+                fontWeight: 700,
+                textOverflow: "ellipsis",
+                overflow: "hidden",
+                whiteSpace: "nowrap"
+              }}
+            >
+              {title}
+            </h1>
+          </Tooltip>
+          <Button style={{ border: "none" }}>
+            <EditOutlined />
+          </Button>
+        </div>
+        <p>
+          <b>Suggested grade level:</b> 8th grade
+        </p>
+        <p>
+          <ClockCircleOutlined /> 15min
+        </p>
+
+        <p>Slides: {slideCount}</p>
+      </div>
+    </LessonPreviewWrapper>
+  );
+};
 
 type ModalProps = {
   title: string;
@@ -110,6 +187,28 @@ export const LessonsPage = (props: PageProps): JSX.Element => {
           onCancel={() => setPageState(PageState.Default)}
         />
       )}
+
+      <LessonsWrapper>
+        <LessonPreview
+          id="0"
+          title="NodeJS read files"
+          slideCount={0}
+          onClick={() => console.log("clicked")}
+        />
+
+        <LessonPreview
+          id="0"
+          title="NodeJS read files"
+          slideCount={0}
+          onClick={() => console.log("clicked")}
+        />
+        <LessonPreview
+          id="0"
+          title="NodeJS read filessaidiaoshdiasndnfia"
+          slideCount={0}
+          onClick={() => console.log("clicked")}
+        />
+      </LessonsWrapper>
     </>
   );
 };
