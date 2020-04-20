7f36c82220e274728411df478d4acc2b8102db99
andres <avald198@fiu.edu>
2020-04-20 13:06:28 -0400

    add response to the multiplechoice object

--- backend/src/app/graphql/objects/blocks/MultipleChoiceQuestionChoice.ts
+++ backend/src/app/graphql/objects/blocks/MultipleChoiceQuestionChoice.ts
@@ -1,4 +1,5 @@
 import { objectType } from 'nexus';
+import prisma from '../../../data/prisma';
 
 export const MultipleChoiceQuestionChoice = objectType({
   name: 'MultipleChoiceQuestionChoice',
@@ -6,5 +7,22 @@ export const MultipleChoiceQuestionChoice = objectType({
     t.model.id();
     t.model.text();
     t.model.correct();
+    t.boolean('selected', {
+      description:
+        'Whether or not the student selected this choice in their answer. Null if the viewer is not a student.',
+      nullable: true,
+      async resolve({ id }, _args, { viewer }: any) {
+        const student = prisma.student.findOne({ where: { id: viewer.id } });
+        if (!student) {
+          return null;
+        }
+        const [response] = await prisma.multipleChoiceQuestionResponse.findMany(
+          {
+            where: { choice: { id }, AND: { student: { id: viewer.id } } },
+          },
+        );
+        return response != null;
+      },
+    });
   },
 });
