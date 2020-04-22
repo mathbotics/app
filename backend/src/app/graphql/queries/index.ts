3295d9114a68c82161bd2155d24314a87b786d03
andres <avald198@fiu.edu>
2020-04-21 21:50:25 -0400

    courses context checking

--- backend/src/app/graphql/queries/index.ts
+++ backend/src/app/graphql/queries/index.ts
@@ -1,11 +1,33 @@
 import { extendType } from 'nexus';
+import { Context } from '../context';
 
 export * from './viewer';
 
 export const query = extendType({
   type: 'Query',
   definition(t) {
-    t.crud.courses({ filtering: true });
+    t.crud.courses({
+      filtering: true,
+      computedInputs: {
+        where({ ctx }) {
+          const { id } = (ctx as Context).viewer;
+          return {
+            instructors: {
+              some: {
+                id,
+              },
+            },
+            OR: {
+              students: {
+                some: {
+                  id,
+                },
+              },
+            },
+          };
+        },
+      },
+    });
     t.crud.lessons({ filtering: true });
     t.crud.lesson();
     t.crud.course();
