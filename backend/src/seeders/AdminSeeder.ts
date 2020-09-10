fce78d01cfee5c39b8c293759c713bfca0c2e3d0
Help <pczerokey@gmail.com>
2020-09-10 12:52:17 -0400

    Added console warning suppression Due to false positive error reporting for eslint : warning  Unexpected console statement  no-console

--- backend/src/seeders/AdminSeeder.ts
+++ backend/src/seeders/AdminSeeder.ts
@@ -3,6 +3,7 @@ import AdminFactory from '../factories/AdminFactory';
 AdminFactory.create()
   .then(() => process.exit(0))
   .catch((e) => {
+      // eslint-disable-next-line no-console
     console.warn(e);
     process.exit(1);
   });
