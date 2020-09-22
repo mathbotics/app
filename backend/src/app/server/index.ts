cbc8bc200dde7082779739d680db23116c5f8ead
verociraptor <vparr007@fiu.edu>
2020-09-22 19:40:21 -0400

    Set files to deploy on heroku

--- backend/src/app/server/index.ts
+++ backend/src/app/server/index.ts
@@ -48,6 +48,12 @@ const apollo = new ApolloServer({
   ),
 });
 
+if(NODE_ENV ==='production'){
+  app.use(express.static(path.join(__dirname, '../../../../frontend/build')));
+  app.use((req, res) => {
+    res.sendFile(path.join(__dirname, '../../../../frontend/build/index.html'));
+  })
+}
 applyExpressMiddlewares(app);
 apollo.applyMiddleware({
   app,
