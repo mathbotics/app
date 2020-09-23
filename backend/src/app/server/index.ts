b62a871ca7cc2654f64afcca3e6c0a722c2f5ac2
verociraptor <vparr007@fiu.edu>
2020-09-22 20:24:10 -0400

    Adapt build scripts to Heroku changes

--- backend/src/app/server/index.ts
+++ backend/src/app/server/index.ts
@@ -49,9 +49,9 @@ const apollo = new ApolloServer({
 });
 
 if(NODE_ENV ==='production'){
-  app.use(express.static(path.join(__dirname, '../../../../frontend/build')));
+  app.use(express.static(path.join(__dirname, '../../../frontend/build')));
   app.use((req, res) => {
-    res.sendFile(path.join(__dirname, '../../../../frontend/build/index.html'));
+    res.sendFile(path.join(__dirname, '../../../frontend/build/index.html'));
   })
 }
 applyExpressMiddlewares(app);
