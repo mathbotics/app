7810d3eed207f5197ee97de92474567098da6298
verociraptor <vparr007@fiu.edu>
2020-10-05 20:22:09 -0400

    Changed delete mutation

--- backend/src/app/graphql/mutations/deleteStudent.ts
+++ backend/src/app/graphql/mutations/deleteStudent.ts
@@ -1,15 +1,31 @@
 import { mutationField, inputObjectType, arg } from 'nexus';
+import nullthrows from 'nullthrows';
 
 import prisma from '../../data/prisma';
 
+export const DeleteStudentInput = inputObjectType({
+    name: 'DeleteStudentInput',
+    definition(t) {
+      t.string('username', {
+        required: true,
+      });
+    },
+  });
 export const deleteStudent = mutationField('deleteStudent',{
     type: 'Student',
-    
+    args: {
+        input: arg({ type: 'DeleteStudentInput', required: true }),
+      },
     async resolve(
-        _root,{input: {username}}
+        _root,{input: {username}},
     ) {
-        await prisma.student.deleteMany({
-          where: {user: username} 
-        })
+       const { count} = nullthrows(
+         await prisma.student.deleteMany({
+          where: {user: 
+            {username}
+          } 
+        }),
+        'Could not create instructor',
+      );
     }
 });
