4c3542373ee391fb8dae11b08fbb7bb3184276db
Help <pczerokey@gmail.com>
2020-10-06 21:53:04 -0400

    Add Carlo's Delete Student Form

---
+++ frontend/src/components/form/DeleteStudentForm.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Form, Button } from 'antd';
+import { Store, ValidateErrorEntity } from 'rc-field-form/lib/interface';
+import { FormItem } from './FormItem';
+import { SelectFormItem } from './SelectFormItem';
+
+type FormProps = {
+    onSubmit: (values: Store) => void;
+    onSubmitError: (error: ValidateErrorEntity) => void;
+};
+
+export type DeleteStudentFormFields = { title: string };
+export const DeleteStudentForm = ({
+                                      onSubmit,
+                                      onSubmitError,
+                                  }: FormProps): JSX.Element => {
+    const [form] = Form.useForm();
+    return (
+        <Form
+            form={form}
+            onFinish={onSubmit}
+            onFinishFailed={onSubmitError}
+            layout='vertical'
+        >
+            <p>Are you sure you wish to delete all students?</p>
+            <p>You cannot undo this action!</p>
+
+            <Button block type="primary" size="large" danger htmlType="submit">
+                Delete All Students
+            </Button>
+        </Form>
+    )
+}
