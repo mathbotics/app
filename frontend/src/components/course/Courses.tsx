6960ff90b8055dd3170fbbd652e08cc2e3c2e6ed
Eberm024 <eberm024@fiu.edu>
2021-03-02 16:13:45 -0500

    saving remotely what I have

--- frontend/src/components/course/Courses.tsx
+++ frontend/src/components/course/Courses.tsx
@@ -13,7 +13,7 @@ import { Courses_query } from './__generated__/Courses_query.graphql';
 
 //import { Courses_user } from './__generated__/Courses_user.graphql';
 import { CoursesUserQuery } from './__generated__/CoursesUserQuery.graphql'; //this was generated because of const UserQuery
-
+import { CoursesList_user } from './__generated__/CoursesList_user.graphql'; //new
 
 import CoursesList from './CoursesList';
 import CreateCourseModal from './CreateCourseModal';
@@ -38,6 +38,7 @@ type Props = { query: Courses_query };
 
 type UserProp = { viewer: CoursesUserQuery };
 //no viewer, second props... that is the original
+
 const Courses = ({ query }: Props, { viewer }: UserProp) => {
   /*
   TODO
@@ -48,20 +49,27 @@ const Courses = ({ query }: Props, { viewer }: UserProp) => {
   const [pageState, setPageState] = useState<PageState>(PageState.Default);
 
 /* Data const with query info creation. */
-  const environment = useRelayEnvironment();
+const environment = useRelayEnvironment();
 
-  const UserQuery = graphql`
-  query CoursesUserQuery {
-    viewer {
-      ...CoursesList_user
-    }
+const UserQuery = graphql`
+query CoursesUserQuery {
+  viewer {
+    ...CoursesList_user
   }
-  `; //need to find a way to make a constant with GraphQLTaggedNode
+}
+`; //need to find a way to make a constant with GraphQLTaggedNode
 
- const data = useLazyLoadQuery<CoursesUserQuery>(UserQuery, {viewer}
- ); //for some reason viewer is undefined... and crashes the app...
+const variables = {
+  
+}; 
 
-  //this has to be some runtime bs error...
+const options = {
+  fetchPolicy: network-only, // no cached data mdoe.. Also to fix issue, I need to call a Network Type?
+};
+
+let data = useLazyLoadQuery<CoursesUserQuery>(UserQuery, variables, options ); //for some reason viewer is undefined... and crashes the app...
+//what if the issue is about how or where I'm assigning data
+//this has to be some runtime bs error...
 
   return (
     <Layout style={{ backgroundColor: 'white', maxHeight: '95vh' }}>
@@ -94,7 +102,7 @@ const Courses = ({ query }: Props, { viewer }: UserProp) => {
       />
 
       {/* loading query for the user below */}
-
+          
 
       {/* Display list of courses */}
       <CoursesList courses={query} user={data?.viewer} />
