0df98fdd2a4cb545901b18d842156cdf5a521d65
Eberm024 <eberm024@fiu.edu>
2021-03-10 01:33:20 -0500

    new format for TextBlock.tsx

--- frontend/src/components/block/text/TextBlock.tsx
+++ frontend/src/components/block/text/TextBlock.tsx
@@ -3,8 +3,31 @@ import { graphql } from 'babel-plugin-relay/macro';
 import { createFragmentContainer } from 'react-relay';
 import { TextBlock_block } from './__generated__/TextBlock_block.graphql';
 
+/* This is the page to start doing customizations to the Slide textblock content */
+
 type Props = { block: TextBlock_block };
-const TextBlock = ({ block }: Props) => <p>{block?.title}</p>;
+
+
+/* Other Display */
+const Title = ({ text }: { text: string }) => (
+  <>
+    <h1>{text}</h1>
+  </>
+  );
+
+ const Body = ({ text }: { text: string}) => (
+  <>
+    <p>{text}</p>
+  </>
+ ); 
+
+/* Main display */
+const TextBlock = ({ block }: Props) => (
+  <>
+    <Title text={block?.title} />
+    <Body text={block?.body} />
+  </>
+);
 
 export default createFragmentContainer(TextBlock, {
   block: graphql`
