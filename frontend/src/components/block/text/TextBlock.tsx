ff9e5780b63c20c13366a6becb2c0fcf25fe939f
Eberm024 <eberm024@fiu.edu>
2021-03-10 14:57:45 -0500

    story done + many changes to text block display

--- frontend/src/components/block/text/TextBlock.tsx
+++ frontend/src/components/block/text/TextBlock.tsx
@@ -1,4 +1,9 @@
 import React from 'react';
+import styled from 'styled-components';
+
+import { Layout, Menu, Tooltip } from 'antd';
+import { DeleteOutlined, PlusOutlined } from '@ant-design/icons';
+
 import { graphql } from 'babel-plugin-relay/macro';
 import { createFragmentContainer } from 'react-relay';
 import { TextBlock_block } from './__generated__/TextBlock_block.graphql';
@@ -7,8 +12,12 @@ import { TextBlock_block } from './__generated__/TextBlock_block.graphql';
 
 type Props = { block: TextBlock_block };
 
-
 /* Other Display */
+
+/*Notes from edgar: if you guys ever plan to work on special text formatting and user selection of fonts, text position,
+or anything else, this would be the place... maybe making the Title and Body have dynamic styles. Those should be on
+the const Title and const Body */
+
 const Title = ({ text }: { text: string }) => (
   <>
     <h1>{text}</h1>
@@ -24,10 +33,41 @@ const Title = ({ text }: { text: string }) => (
 /* Main display */
 const TextBlock = ({ block }: Props) => (
   <>
-    <Title text={block?.title} />
-    <Body text={block?.body} />
+  <SlideWrapper>
+    <TitleWrapper>
+      <Title text={block?.title} />
+    </TitleWrapper>
+    <BodyWrapper>
+      <Body text={block?.body} />
+    </BodyWrapper>
+  </SlideWrapper>
   </>
 );
+
+/* CSS formats */
+const TitleWrapper = styled.div`
+  position: relative;
+  width: 100%;
+  height: 10%;
+  padding: 10px 10px 10px 10px;
+  display: content;
+  font-size: 24px;
+`;
+
+const BodyWrapper = styled.div`
+  
+  width: 100%;
+  height: 90%;
+  padding: 10px 10px 10px 10px;
+
+  display: content;
+`;
+
+const SlideWrapper = styled.div`
+  position: relative;
+  width: 100%;
+  height: 100%;
+`;
 
 export default createFragmentContainer(TextBlock, {
   block: graphql`
